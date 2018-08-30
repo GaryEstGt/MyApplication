82ecacbf45ebea0ae47717a215310c1153cd9a3f
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-08-29 20:44:34 -0600

    probando si funciona

--- app/src/main/java/com/example/garya/myapplication/MainActivity.java
+++ app/src/main/java/com/example/garya/myapplication/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.garya.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etNombre)
+    EditText etNombre;
+    @BindView(R.id.etpellido)
+    EditText etpellido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         //probdo si funcionss otrvez
         //lelelelelelelelelelele
+    }
+
+    @OnClick(R.id.btn1)
+    public void onViewClicked() {
+        String Nombre="";
+        Nombre= etNombre.getText().toString();
+        String pellido;
+        pellido=etpellido.getText().toString();
+
     }
 }
